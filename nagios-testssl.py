2877f5472f7542c5f9d5313a3adcf0d191ea282a
Dick Visser <dick.visser@geant.org>
2020-03-26 15:35:29 +0100

    release ready

--- nagios-testssl.py
+++ nagios-testssl.py
@@ -4,9 +4,8 @@ import os
 import sys
 import tempfile
 import subprocess
-import re
 import json
-import pyjq
+import jmespath
 from pprint import pprint
 from urllib.parse import urlparse
 
@@ -31,7 +30,8 @@ try:
             choices=severities.keys(), default='CRITICAL')
     parser.add_argument('--warning', help='Findings of this severity level trigger a WARNING', 
             choices=severities.keys(), default='HIGH')
-    parser.add_argument('args', nargs=argparse.REMAINDER)
+    # FIXME this is unreliable
+    parser.add_argument('trailing_args', nargs=argparse.REMAINDER)
     args = parser.parse_args()
 
     if severities[args.critical] < severities[args.warning]:
@@ -44,12 +44,19 @@ try:
     testssl = args.testssl
     critical = args.critical
     warning = args.warning
-
+    trailing_args = args.trailing_args
+    # pprint(args)
 
     # start with clean slate
     ok_msg = []
     warn_msg = []
     crit_msg = []
+
+    msg = {
+            'ok': [],
+            'warning': [],
+            'critical': []
+            }
 
     # Create temp file
     fd, temp_path = tempfile.mkstemp()
@@ -62,28 +69,24 @@ try:
         uri
         ]
 
-    # Inject this script's extra command line arguments before the 'uri' part of the testssl.sh command
-    for extra in args.args:
+    # Inject this script's trailing command line arguments before the 'uri' part of
+    # the testssl.sh command.
+    for extra in trailing_args:
         subproc_args.insert(3, extra)
 
     # Run it
     proc = subprocess.run(subproc_args, stdout=subprocess.PIPE)
-    # pprint(proc.returncode)
-    # pprint(proc)
 
-    # temp_path = os.path.expanduser("~/careers.deloitte.ca_p443-20200325-1434.json")
-    # temp_path = os.path.expanduser("~/www.geant.org_p443-20200325-1857.json")
     with open(temp_path) as f:
         json = json.load(f)
     os.close(fd)
     os.remove(temp_path)
 
-    r = pyjq.all('.scanResult[] | .[]? []?', json)
+    r = jmespath.search('scanResult[].[*][*]|[0][0][][]|[?severity]', json)
 
     # Add integer severity level
     for item in r:
         item['severity_int'] = severities[item['severity']]
-
 
     def get_severity_count_aggregated(severity_int):
         return len([f for f in r if f['severity_int'] >= severity_int])
@@ -92,32 +95,33 @@ try:
         _results = sorted([f for f in r if f['severity_int'] >= severity_int], key = lambda i: i['severity_int'], reverse=True)
         return list(map(lambda x: x['severity'] +  ": " + x['id'] + " (" + x['finding'] + ")", _results))
 
-    # FIXME loop over these?
     if get_severity_count_aggregated(severities[critical]) > 0:
-        crit_msg.append("{0} issues found for {1} with severity {2} or higher: {3}".format(
+        msg['critical'].append("{0} issues found for {1} with severity {2} or higher.\n{3}".format(
             get_severity_count_aggregated(severities[critical]),
             uri,
             critical,
-            ', '.join(get_severity_items_aggregated(severities[critical])),
-
+            '\n'.join(get_severity_items_aggregated(severities[critical])),
             ))
     if get_severity_count_aggregated(severities[warning]) > 0:
-        warn_msg.append("{0} issues found for {1} with severity {2} or higher: {3}".format(
+        msg['warning'].append("{0} issues found for {1} with severity {2} or higher.\n{3}".format(
             get_severity_count_aggregated(severities[warning]),
             uri,
             warning,
-            ', '.join(get_severity_items_aggregated(severities[warning])),
+            '\n'.join(get_severity_items_aggregated(severities[warning])),
             ))
     else:
-        # FIXME display number of lower severity issues? To show that things did run.
-        ok_msg.append("No issues found with severity {0} or higher".format(warning))
+        msg['ok'].append("No issues found for {0} with severity {1} or higher.".format(
+            uri,
+            warning,
+            ))
+
 except Exception as e:
     nagios_exit("UNKNOWN: Unknown error: {0}.".format(e), 3)
 
 # Exit with accumulated message(s)
-if crit_msg:
-    nagios_exit("CRITICAL: " + ' '.join(crit_msg), 2)
-elif warn_msg:
-    nagios_exit("WARNING: " + ' '.join(warn_msg), 1)
+if len(msg['critical']) > 0:
+    nagios_exit("CRITICAL: " + ' '.join(msg['critical']), 2)
+elif len(msg['warning']) > 0:
+    nagios_exit("WARNING: " + ' '.join(msg['warning']), 1)
 else:
-    nagios_exit("OK: " + ' '.join(ok_msg), 0)
+    nagios_exit("OK: " + ' '.join(msg['ok']), 0)
